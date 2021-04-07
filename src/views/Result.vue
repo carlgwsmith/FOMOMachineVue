766f8f355aeecb758cd5599a9c2f2eec0283206d
Carl Smith <carlsmith@socialsecuritysolutions.com>
2021-04-07 17:08:37 -0500

    Update Result.vue

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -3,6 +3,7 @@
     Symbol: {{ticker}}
     Today's Price: {{stockPrice}}
     First Traded on: {{firstTradeDateReadable}}
+    {{getIpo}}
 
 </div>
 </template>
@@ -14,6 +15,7 @@ export default {
         stockPrice:0,
         firstTradeDate:0,
         firstTradeDateReadable:''
+        ipoClosePrice: 0,
     }),
     computed: {
     returnState () {
@@ -31,6 +33,29 @@ export default {
             this.firstTradeDateReadable = myDate.toLocaleString();
     },
     methods: {
+        getIpo (){
+            //converter firstTradeDate for use in API
+            var s = new Date(this.firstTradeDate).toISOString();
+            var res = s.substring(0, 10)
+            console.log('RES: '+ res);
+
+            const options = {
+            method: 'GET',
+            //url: 'https://eodhistoricaldata.com/api/eod/' + this.ticker + '.US?api_token=606d1b402eb7b6.11619858',
+            url:'https://eodhistoricaldata.com/api/eod/'+this.ticker+'.US?from='+res+'&to='+res+'&api_token=606d1b402eb7b6.11619858&period=d&fmt=json'
+            headers: {
+                'x-rapidapi-key': '576a270f4emshce03cc0d892e394p15648fjsnddb66ef301e9',
+                'x-rapidapi-host': 'eod-historical-data.p.rapidapi.com'
+            }
+            };
+
+            axios.request(options).then(function (response) {
+                console.log(response.data);
+
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
     }
 
 }
