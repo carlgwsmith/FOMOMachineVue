00e5865c090b227f17535ebc1d1a8ce197c13ad6
Carl Smith <carlsmith@socialsecuritysolutions.com>
2021-08-03 08:25:31 -0500

    Update historyChartView.vue

--- src/components/historyChartView.vue
+++ src/components/historyChartView.vue
@@ -5,7 +5,7 @@
 </template>
 
 // <script>
-import HistoryChart from './charts/HistoryChart.vue';
+import HistoryChart from './charts/HistoryChart.vue'
 
 export default {
     Name: 'Longevity Results',
